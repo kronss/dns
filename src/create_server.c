a25580537aac0cca4c57dd9eb6153f7cbb6a8cd9
Oleg Chaychenko <oleg.chaychenko@gmail.com>
2017-08-15 00:56:47 +0300

    hotfix, suport std port

--- src/create_server.c
+++ src/create_server.c
@@ -4,7 +4,9 @@ int						create_server(void)
 {
 	int					sockfd;
 	struct sockaddr_in	server;
-													
+	int					enable;
+
+
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		err_msg("socket() failed");
 
@@ -13,8 +15,15 @@ int						create_server(void)
 	server.sin_addr.s_addr = INADDR_ANY;
 
 
-	// server.sin_port = htons(53);
-	server.sin_port = htons(5555); 
+	// server.sin_port = htons(53); // std port
+	server.sin_port = htons(5555);  // test port
+
+	enable = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1)
+	{
+    	close(sockfd);
+    	err_msg("setsockopt(SO_REUSEADDR) failed");
+	}
 
 	if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1)
 	{
